b1641c55e314a619f4bf04f200cc59231f7319b5
Willowispll <gorelikgleb@gmail.com>
2026-03-29 23:14:15 +0300

    [tlp] added and conf

--- flake.nix
+++ flake.nix
@@ -47,5 +47,5 @@
     };
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
--- modules/hosts/amygdala/amygdala.nix
+++ modules/hosts/amygdala/amygdala.nix
@@ -13,6 +13,7 @@ in {
       packages
       niri
       amnezia
+      tlp
       homeManager
     ];
     homeModules = with self.homeModules; [
